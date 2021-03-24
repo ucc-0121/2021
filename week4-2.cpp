f1177723bb6f903db349815a2fa5de1932c504dc
ucc-0121 <79627932+ucc-0121@users.noreply.github.com>
2021-03-24 23:02:21 +0800

    Add files via upload

---
+++ week4-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+struct DATA{
+    float x,y,z;
+}point1;
+int main()
+{
+    point1.x=3;
+    point1.y=5;
+    point1.z=7;
+    printf("%f %f %f",point1.x,point1.y,point1.z);
+}
+
