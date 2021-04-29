33000afe7de9efe930284969906fac6344695d73
ucc-0121 <79627932+ucc-0121@users.noreply.github.com>
2021-04-29 17:40:30 +0800

    Rename week 10-5(1).cpp to week10/week 10-5(1).cpp

--- week10/week 10-5(1).cpp
+++ week10/week 10-5(1).cpp
@@ -11,7 +11,7 @@ int compare(const void *p1,const void *p2){
 	else if( ((BOX*)p1)->ans < ((BOX*)p2)->ans) return +1;
 	else if( ((BOX*)p1)->c   < ((BOX*)p2)->c) return -1 ;
 	else if( ((BOX*)p1)->c   > ((BOX*)p2)->c) return +1 ;
-	else return 0;//�۵��ɤ�r��
+	else return 0;//¬Ûµ¥®É¤ñ¦r¥À
 }
 int main()
 {
