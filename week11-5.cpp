c4b712abf14b675bd44710801652630d9bf84e69
ucc-0121 <79627932+ucc-0121@users.noreply.github.com>
2021-05-06 17:13:45 +0800

    Add files via upload

---
+++ week11-5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+char line[2000];
+int main()
+{
+	for(int t=0;get(line)!=NULL;t++){
+		if(t>0) printf("\n");
+		printf("blahblah");
+		printf("blahblah");
+		printf("blahblah");
+	}
+}
